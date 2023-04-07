17cf94f464abcf26d3b7cf331e086969a23282a8
kauannre <130103202+kauannre@users.noreply.github.com>
2023-04-07 17:48:32 -0300

    Create index.js

---
+++ plugins/hidetyping/index.js
@@ -0,0 +1,15 @@
+export default {
+		onLoad: function () {
+			
+
+import { findByProps } from 'vendetta/metro';
+import { before } from "vendetta/utils/patcher";
+
+const typing = findByProps("startTyping");
+
+        before(typing, "startTyping", ctx => {
+            ctx.result = null
+        })
+
+		},
+	};
