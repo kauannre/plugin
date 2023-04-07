20c8c4cb1177bfbc1cd181b781f7f0c511a100ef
kauannre <130103202+kauannre@users.noreply.github.com>
2023-04-07 19:42:55 -0300

    Update index.js

--- plugins/hidetyping/index.js
+++ plugins/hidetyping/index.js
@@ -1,10 +1,10 @@
 export default {
 		onLoad: function () {
-		const { metro, utils } = vendetta;
+		const { metro, utils, patcher } = vendetta;
 		
 		const { startTyping } = metro.findByProps("startTyping");
 		
-		utils.patcher.before(typing, "startTyping", ctx => {
+		patcher.before(typing, "startTyping", ctx => {
             ctx.result = null
         });
         }};
