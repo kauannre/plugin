663255ef210341ab2dedcf9a1af7811c9400efae
kauannre <kauannre@users.noreply.github.com>
2023-04-07 22:03:24 +0000

    deploy: cfd505ab5404f96810b9ea1a0312f55baed3012d

--- hidetyping/index.js
+++ hidetyping/index.js
@@ -1,1 +1,1 @@
-(function(t,e,n){"use strict";var r={onLoad:function(){const u=e.findByProps("startTyping");n.before(u,"startTyping",function(i){i.result=null})}};return t.default=r,Object.defineProperty(t,"__esModule",{value:!0}),t})({},metro,patcher);
+(function(t){"use strict";var e={onLoad:function(){const{metro:n,utils:r}=vendetta,u=n.findByProps("startTyping");r.patcher.before(u,"startTyping",function(i){i.result=null})}};return t.default=e,Object.defineProperty(t,"__esModule",{value:!0}),t})({});
