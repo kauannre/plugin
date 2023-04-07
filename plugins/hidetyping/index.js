03d155d69fe4e3b51c317125047a55571787cec0
kauannre <130103202+kauannre@users.noreply.github.com>
2023-04-07 17:38:52 -0300

    Update index.js

--- plugins/hidetyping/index.js
+++ plugins/hidetyping/index.js
@@ -2,10 +2,10 @@ export default {
 		onLoad: function () {
 			
 			import { Plugin } from "vendetta/entities";
-import { getByProps } from 'vendetta/metro';
+import { findByProps } from 'vendetta/metro';
 import { before } from "vendetta/utils/patcher";
 
-const typing = getByProps("startTyping");
+const typing = findByProps("startTyping");
 
         before(typing, "startTyping", ctx => {
             ctx.result = null
