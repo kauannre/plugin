20afdfc90e34ee6d924bc2b53b10f20c9a23f850
kauannre <130103202+kauannre@users.noreply.github.com>
2023-04-09 14:59:58 -0300

    Create index.ts

---
+++ plugins/imunidade/index.ts
@@ -0,0 +1,36 @@
+import { findByName } from "@vendetta/metro";
+import { before, after } from "@vendetta/patcher";
+import { Embed, Message } from "vendetta-extras";
+
+const patches = [];
+
+const RowManager = findByName("RowManager");
+
+patches.push(before("generate", RowManager.prototype, ([data]) => {
+  if (data.rowType !== 1) return;
+
+  let content = data.message.content as string;
+  if (!content?.length) return;
+
+  // Replace "oi" with "teste" in content
+  content = content.replace(/oi/g, "teste");
+  data.message.content = content;
+}));
+
+patches.push(after("generate", RowManager.prototype, ([data], row) => {
+  if (data.rowType !== 1) return;
+
+  const { content } = row.message as Message;
+  if (!Array.isArray(content)) return;
+
+  // Replace "oi2" with "teste2" in content
+  const newContent = content.map((c) => {
+    if (c.type === "text" && c.content === "oi2") {
+      return { type: "text", content: "teste2" };
+    }
+    return c;
+  });
+  row.message.content = newContent;
+}));
+
+export const onUnload = () => patches.forEach((unpatch) => unpatch());
