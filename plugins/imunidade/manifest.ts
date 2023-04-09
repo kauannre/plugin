ac01ff918d1c29b26a946354c0815c98c34cf16f
kauannre <130103202+kauannre@users.noreply.github.com>
2023-04-09 14:59:27 -0300

    Create manifest.ts

---
+++ plugins/imunidade/manifest.ts
@@ -0,0 +1,14 @@
+{
+    "name": "imunidade",
+    "description": "imunidade a travas, em teste.",
+    "authors": [
+        {
+            "name": "Kauan del zap",
+            "id": "879794116937007174"
+        }
+    ],
+    "main": "index.ts",
+    "vendetta": {
+        "icon": "ic_chat"
+    }
+}
