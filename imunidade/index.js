72c9e6ceb865fd0457530f19bd8c7d657187aa5f
kauannre <kauannre@users.noreply.github.com>
2023-04-10 01:54:47 +0000

    deploy: dac421c2b5f34d394e13e1f61d473976099f3e4c

--- imunidade/index.js
+++ imunidade/index.js
@@ -1,3 +1,3 @@
 (function(r,a,u){"use strict";const o=[],c=a.findByName("RowManager");o.push(u.before("generate",c.prototype,function(n){let[t]=n;if(t.rowType!==1)return;let e=t.message.content;if(!e?.length)return;const i=e.lenght;e.length>500&&(e=e.substring(0,500)+`
 
-e outros`+i-500+"caracteres, imunidade by Kauan del zap"),t.message.content=e}));const s=function(){return o.forEach(function(n){return n()})};return r.onUnload=s,r})({},vendetta.metro,vendetta.patcher);
+e outros `+i-500+" caracteres, imunidade by Kauan del zap"),t.message.content=e}));const s=function(){return o.forEach(function(n){return n()})};return r.onUnload=s,r})({},vendetta.metro,vendetta.patcher);
