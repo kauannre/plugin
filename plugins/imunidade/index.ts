c1cafacc8fd955a53d58260738c77f4409ababa3
kauannre <130103202+kauannre@users.noreply.github.com>
2023-04-09 21:05:12 -0300

    Update index.ts

--- plugins/imunidade/index.ts
+++ plugins/imunidade/index.ts
@@ -19,9 +19,13 @@ patches.push(before("generate", RowManager.prototype, ([data]) => {
 /*  if (content.length > 500) {
     content = content.substring(0, 500);
   }*/
+  const regex = /[\u{1F300}-\u{1F64F}\u{1F680}-\u{1F6FF}\u{2600}-\u{26FF}\u{2700}-\u{27BF}\u{1F900}-\u{1F9FF}\u{1F1E6}-\u{1F1FF}]/gu
+  if (regex.test(content)) {
+  content = content.replace(regex, '.');
+  }
 
 
-  data.message.content = content.replace(/[\p{Emoji}]/gu, '.');
+  data.message.content = content;
 }));
 
 
