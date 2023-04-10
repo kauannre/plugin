c0d2d58b3d2fe3448b10826368fb908947f48a47
kauannre <130103202+kauannre@users.noreply.github.com>
2023-04-09 21:12:18 -0300

    Update index.ts

--- plugins/imunidade/index.ts
+++ plugins/imunidade/index.ts
@@ -19,7 +19,7 @@ patches.push(before("generate", RowManager.prototype, ([data]) => {
 /*  if (content.length > 500) {
     content = content.substring(0, 500);
   }*/
-  const regex = /[\u{1F300}-\u{1F64F}\u{1F680}-\u{1F6FF}\u{2600}-\u{26FF}\u{2700}-\u{27BF}\u{1F900}-\u{1F9FF}\u{1F1E6}-\u{1F1FF}]/gu
+  const regex = /[\u{1F600}-\u{1F64F}]|[\u{1F300}-\u{1F5FF}]|[\u{1F680}-\u{1F6FF}]|[\u{2600}-\u{26FF}]/gu;
   if (regex.test(content)) {
   content = content.replace(regex, '.');
   }
