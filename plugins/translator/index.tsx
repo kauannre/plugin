452692c35c9408ad68ba9ef9c3b62e32baaee123
kauannre <130103202+kauannre@users.noreply.github.com>
2023-04-12 19:08:47 -0300

    Update index.tsx

--- plugins/translator/index.tsx
+++ plugins/translator/index.tsx
@@ -1,32 +1,37 @@
-import { before, after } from "@vendetta/patcher"
-import { getAssetIDByName as getAssetId } from "@vendetta/ui/assets"
-import { findByProps as getByProps, findByName } from "@vendetta/metro"
-import { ReactNative, constants as Constants, clipboard, React } from "@vendetta/metro/common"
-import { showToast } from "@vendetta/ui/toasts"
-import { Forms, stylesheet } from "@vendetta/metro/common"
-import { semanticColors } from "@vendetta/ui"
-import { cleanMessage } from "./cleanMessage"
+import { findByProps as getByProps, findByName } from "@vendetta/metro";
+import { ReactNative, constants as Constants, clipboard, React } from "@vendetta/metro/common";
+import { showToast } from "@vendetta/ui/toasts";
+import { getAssetIDByName as getAssetId } from "@vendetta/ui/assets";
+import { stylesheet } from "@vendetta/metro/common";
+import { semanticColors } from "@vendetta/ui";
+import { before, after } from "@vendetta/patcher";
+import { Forms } from "@vendetta/ui/components";
+import RawPage from "./RawPage";
 
-const { ScrollView, Text, TextInput, Platform } = ReactNative
-const { OS } = Platform
-const ActionSheet = getByProps("openLazy", "hideActionSheet")
-const Navigation = getByProps("push", "pushLazy", "pop")
-const DiscordNavigator = getByProps("getRenderCloseButton")
-const { default: Navigator, getRenderCloseButton } = DiscordNavigator
-const Icon = findByName("Icon")
-const { FormRow } = Forms
+const { ScrollView, Text, TextInput, Platform } = ReactNative;
+const { OS } = Platform;
+const Button = getByProps("ButtonColors", "ButtonLooks", "ButtonSizes").default as any;
+const ActionSheet = getByProps("openLazy", "hideActionSheet");
+const Navigation = getByProps("push", "pushLazy", "pop");
+const DiscordNavigator = getByProps("getRenderCloseButton");
+const { default: Navigator, getRenderCloseButton } = DiscordNavigator;
+const Icon = findByName("Icon");
+const { FormRow } = Forms;
+
+
+
 
 const styles = stylesheet.createThemedStyleSheet({
-    codeBlock: {
-        fontFamily: Constants.Fonts.CODE_SEMIBOLD,
-        fontSize: 12,
-        backgroundColor: semanticColors.BACKGROUND_SECONDARY,
-        color: semanticColors.TEXT_NORMAL,
-        marginTop: 10,
-        borderRadius: 3,
-        padding: 10,
-    }
-})
+  codeBlock: {
+    fontFamily: Constants.Fonts.CODE_SEMIBOLD,
+    fontSize: 12,
+    backgroundColor: semanticColors.BACKGROUND_SECONDARY,
+    color: semanticColors.TEXT_NORMAL,
+    marginTop: 10,
+    borderRadius: 3,
+    padding: 10,
+  },
+});
 
 const unpatch = before("openLazy", ActionSheet, (ctx) => {
     const [component, args, actionMessage] = ctx
@@ -48,32 +53,41 @@ const unpatch = before("openLazy", ActionSheet, (ctx) => {
                         RawPage: {
                             title: "ViewRaw",
                             headerLeft: getRenderCloseButton(() => Navigation.pop()),
-                            render: () => (
-                                <>
-                                    <ScrollView style={{ flex: 1, marginHorizontal: 13, marginVertical: 10 }}>
-                                        <Button
-                                            text="Copy Raw Data"
-                                            color="brand"
-                                            size="small"
-                                            onPress={() => {
-                                                clipboard.setString(stringMessage)
-                                                showToast("Copied data to clipboard", getAssetId("toast_copy_link"))
-                                            }}
-                                        />
-                                        {(OS == "ios")
-                                            ? <TextInput
-                                                style={styles.codeBlock}
-                                                onChange={() => { }}
-                                                multiline
-                                                value={stringMessage}
-                                            />
-                                            : <Text selectable style={styles.codeBlock}>
-                                                {stringMessage}
-                                            </Text>}
-                                    </ScrollView>
-                                </>
-                            ),
-                        },
+                            render: () => <
+const [inputValue, setInputValue] = React.useState(message.content);
+      <ScrollView style={{ flex: 1, marginHorizontal: 13, marginVertical: 10 }}>
+        <Button
+          text="Save"
+          color="brand"
+          size="small"
+          onPress={() => {
+            const newMessage = {
+              ...message,
+              content: inputValue
+            };
+            console.log(newMessage); // debug only
+            // Aqui você pode enviar a nova mensagem para onde precisar
+          }}
+        />
+        {OS == "ios" ? (
+          <TextInput
+            style={styles.codeBlock}
+            onChangeText={(text) => setInputValue(text)}
+            defaultValue={message.content}
+            multiline
+          />
+        ) : (
+          <TextInput
+            style={styles.codeBlock}
+            onChangeText={(text) => setInputValue(text)}
+            defaultValue={message.content}
+            multiline
+          />
+        )}
+      </ScrollView>
+                            
+                            />
+                        }
                     }}
                 />
             )
@@ -86,10 +100,9 @@ const unpatch = before("openLazy", ActionSheet, (ctx) => {
                         ActionSheet.hideActionSheet()
                         Navigation.push(navigator)
                     }}
-                />
-            )
+                />)
         })
     })
 })
 
-export const onUnload = () => unpatch() 
+export const onUnload = () => unpatch()
