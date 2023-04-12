e22268a436226857b20d6da28af50cb9e2907367
kauannre <130103202+kauannre@users.noreply.github.com>
2023-04-12 17:18:35 -0300

    Update RawPage.tsx

--- plugins/translator/RawPage.tsx
+++ plugins/translator/RawPage.tsx
@@ -1,13 +1,13 @@
-import { findByProps as getByProps } from "@vendetta/metro"
-import { ReactNative, constants as Constants, clipboard, React } from "@vendetta/metro/common"
-import { showToast } from "@vendetta/ui/toasts"
-import { getAssetIDByName as getAssetId } from "@vendetta/ui/assets"
-import { stylesheet } from "@vendetta/metro/common"
-import { semanticColors } from "@vendetta/ui"
+import { findByProps as getByProps } from "@vendetta/metro";
+import { ReactNative, constants as Constants, clipboard, React } from "@vendetta/metro/common";
+import { showToast } from "@vendetta/ui/toasts";
+import { getAssetIDByName as getAssetId } from "@vendetta/ui/assets";
+import { stylesheet } from "@vendetta/metro/common";
+import { semanticColors } from "@vendetta/ui";
 
-const { ScrollView, Text, TextInput, Platform } = ReactNative
-const { OS } = Platform
-const Button = getByProps("ButtonColors", "ButtonLooks", "ButtonSizes").default as any
+const { ScrollView, Text, TextInput, Platform } = ReactNative;
+const { OS } = Platform;
+const Button = getByProps("ButtonColors", "ButtonLooks", "ButtonSizes").default as any;
 
 const styles = stylesheet.createThemedStyleSheet({
   codeBlock: {
@@ -19,36 +19,40 @@ const styles = stylesheet.createThemedStyleSheet({
     borderRadius: 3,
     padding: 10,
   }
-})
+});
 
-interface Props {
+interface RawPageProps {
   message: any;
-  onSave: (messageId: string, newContent: string) => void;
+  onSave: (newContent: string) => void;
 }
 
-export default function RawPage({ message, onSave }: Props) {
-  const [rawContent, setRawContent] = React.useState(JSON.stringify(message, null, 4));
-
-  const handleSave = React.useCallback(() => {
-    onSave(message.id, rawContent);
-    showToast("Saved changes", getAssetId("toast_copy_link"));
-  }, [message.id, rawContent, onSave]);
+export default function RawPage({ message, onSave }: RawPageProps) {
+  const [content, setContent] = React.useState<string>(message.content);
 
   return (
     <>
       <ScrollView style={{ flex: 1, marginHorizontal: 13, marginVertical: 10 }}>
         <Button
           text="Save"
-          color="green"
+          color="brand"
           size="small"
-          onPress={handleSave}
+          onPress={() => {
+            onSave(content);
+            showToast("Content saved", getAssetId("toast_copy_link"));
+          }}
         />
-        <TextInput
-          style={styles.codeBlock}
-          multiline
-          value={rawContent}
-          onChangeText={setRawContent}
-        />
+        {(OS == "ios") ? (
+          <TextInput
+            style={styles.codeBlock}
+            multiline
+            value={content}
+            onChangeText={(text) => setContent(text)}
+          />
+        ) : (
+          <Text selectable style={styles.codeBlock}>
+            {content}
+          </Text>
+        )}
       </ScrollView>
     </>
   );
