c01e0fdcd3107f64b8885c473bbe6f04dc0dd2b5
kauannre <130103202+kauannre@users.noreply.github.com>
2023-04-12 17:19:23 -0300

    Update index.tsx

--- plugins/translator/index.tsx
+++ plugins/translator/index.tsx
@@ -40,12 +40,11 @@ const unpatch = before("openLazy", ActionSheet, (ctx) => {
 
             buttons.push(
                 <FormRow
-                    label="Save"
+                    label="View Raw"
                     leading={<Icon source={getAssetId("ic_chat_bubble_16px")} />}
                     onPress={() => {
                         ActionSheet.hideActionSheet()
-                        message.content = document.querySelector('textarea')?.value ?? message.content
-                        console.log(message.content) // para checar se o conteúdo foi atualizado corretamente
+                        Navigation.push(navigator)
                     }}
                 />)
         })
