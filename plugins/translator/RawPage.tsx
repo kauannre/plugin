d88aaa16d055f8b0f58dcbae01a5262c8cfca9a3
kauannre <130103202+kauannre@users.noreply.github.com>
2023-04-12 18:03:12 -0300

    Update RawPage.tsx

--- plugins/translator/RawPage.tsx
+++ plugins/translator/RawPage.tsx
@@ -2,7 +2,6 @@ import { findByProps as getByProps } from "@vendetta/metro";
 import { ReactNative, constants as Constants, clipboard, React } from "@vendetta/metro/common";
 import { showToast } from "@vendetta/ui/toasts";
 import { getAssetIDByName as getAssetId } from "@vendetta/ui/assets";
-import { cleanMessage } from "./cleanMessage";
 import { stylesheet } from "@vendetta/metro/common";
 import { semanticColors } from "@vendetta/ui";
 
@@ -23,38 +22,38 @@ const styles = stylesheet.createThemedStyleSheet({
 });
 
 export default function RawPage({ message }) {
-  const stringMessage = React.useMemo(() => JSON.stringify(cleanMessage(message), null, 4), [message.id]);
-  const [inputValue, setInputValue] = React.useState(message.content); // inicializa o valor do estado local com o conteúdo atual da mensagem
+  const [inputValue, setInputValue] = React.useState('');
 
   return (
     <>
-      <ScrollView style={{ flex: 1, marginHorizontal: 13, marginVertical: 10 }}>
-        <Button
-          text="Save"
-          color="brand"
-          size="small"
-          onPress={() => {
-            const newMessage = {
-              ...message,
-              content: inputValue // atualiza o conteúdo da mensagem com o valor atual de inputValue
-            };
-            console.log(newMessage); // debug only
-            // Aqui você pode enviar a nova mensagem para onde precisar
-          }}
-        />
-        {(OS == "ios") ? (
-          <TextInput
-            style={styles.codeBlock}
-            value={inputValue} // vincula o valor do TextInput ao estado local inputValue
-            onChangeText={(text) => setInputValue(text)} // atualiza o estado local inputValue sempre que o texto do TextInput mudar
-            multiline
-          />
-        ) : (
-          <Text selectable style={styles.codeBlock}>
-            {stringMessage}
-          </Text>
-        )}
-      </ScrollView>
+<ScrollView style={{ flex: 1, marginHorizontal: 13, marginVertical: 10 }}>
+  <Button
+    text="Save"
+    color="brand"
+    size="small"
+    onPress={() => {
+      const newMessage = {
+        ...message,
+        content: inputValue
+      };
+      console.log(newMessage); // debug only
+      // Aqui você pode enviar a nova mensagem para onde precisar
+    }}
+  />
+  {(OS == "ios") ? (
+    <TextInput
+      style={styles.codeBlock}
+      onChange={(event) => setInputValue(event.nativeEvent.text)}
+      multiline
+      defaultValue={message.content} // utilize defaultValue em vez de value
+    />
+  ) : (
+    <Text selectable style={styles.codeBlock}>
+      {JSON.stringify(message, null, 4)}
+    </Text>
+  )}
+</ScrollView>
+
     </>
   );
 }
