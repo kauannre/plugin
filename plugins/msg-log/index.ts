b737b5148398e882f825b31c86b8081d73d3b4a3
kauannre <130103202+kauannre@users.noreply.github.com>
2023-04-15 00:19:14 -0300

    Create index.ts

--- plugins/msg-log/index.ts
+++ plugins/msg-log/index.ts
@@ -25,9 +25,9 @@ patches.push(before("actionHandler", FD.MESSAGE_UPDATE?.find(i => i.name === "Me
             
 
 
-/*
+
 patches.push(before("actionHandler", FD.PASSIVE_UPDATE_V1?.find(i => i.name === "MessageStore"), (args: any) => {
-console.log(args)
+//console.log(args)
 /*
                 try {
                 
@@ -45,7 +45,7 @@ MessageActions.receiveMessage(args[0].message.channel_id, msg);
                 //    args[0].message.content = msgantiga + " `[edited]`\n" + message;
                     
             } catch (e) {}*/
-      //      }));
+            }));
 
 
 
