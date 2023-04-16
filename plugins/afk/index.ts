9b398807ffdf1360cf5e894f280af9e9b85cb5ee
kauannre <130103202+kauannre@users.noreply.github.com>
2023-04-16 01:20:22 -0300

    Update index.ts

--- plugins/afk/index.ts
+++ plugins/afk/index.ts
@@ -11,11 +11,27 @@ const FD = FluxDispatcher._actionHandlers._orderedActionHandlers;
 //const Channels = findByProps('getLastSelectedChannelId')
 //const BotMessage = findByProps('createBotMessage')
 //const Avatars = findByProps("BOT_AVATARS")
+const meuid = findByProps("getCurrentUser").getCurrentUser().id
+
+const MessageActions = findByProps("sendMessage", "receiveMessage")
 
 let patches = []
 
 patches.push(before("actionHandler", FD.MESSAGE_CREATE?.find(i => i.name === "MessageStore"), (args: any) => {
-console.log(storage.afk)
+let message = args[0].message;
+let guildId = args[0].guildId;
+let channelId = args[0].channelId;
+if(message.content.includes("<@" + meuid + ">")) {
+MessageActions.sendMessage(channelId, {
+                content: storage.afk,
+                "messageReference": {
+        "message_id": message.id,
+        "guild_id": guildId,
+        "channel_id": channelId
+    }
+            });
+
+}
                 
             }));
             
@@ -29,3 +45,20 @@ export const onLoad = () => {
 export const onUnload = () => {
     for (const unregisterCommands of patches) unregisterCommands()
 }
+
+/*
+/eval code:const { findByProps,findByName} = vendetta.metro;
+const CurrentUserStore = findByProps("getCurrentUser")
+JSON.stringify(CurrentUserStore.getCurrentUser())
+
+MessageActions.sendMessage(id, {
+                content: mensagem.value,
+                "messageReference": {
+        "channel_id": "1096955219834835004",
+        "guild_id": "1096955219386052628",
+        "message_id": "1097010107910459522"
+    },
+            });
+            
+            
+*/
