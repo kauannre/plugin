36a6b0b7e87b92819400141472950028cc21a38a
kauannre <130103202+kauannre@users.noreply.github.com>
2023-04-16 00:23:14 -0300

    Create index.ts

---
+++ plugins/afk/index.ts
@@ -0,0 +1,31 @@
+// some of the code taken from aeongdesu & original emnity plugin by spinfal (most of the code is from spinfal)
+import { registerCommand } from "@vendetta/commands"
+import { logger } from "@vendetta";
+import { findByProps } from "@vendetta/metro"
+import Settings from "./settings";
+import { storage } from '@vendetta/plugin';
+const MessageActions = findByProps("sendMessage", "receiveMessage")
+import { FluxDispatcher } from '@vendetta/metro/common';
+import { before, after} from "@vendetta/patcher"
+const FD = FluxDispatcher._actionHandlers._orderedActionHandlers;
+//const Channels = findByProps('getLastSelectedChannelId')
+//const BotMessage = findByProps('createBotMessage')
+//const Avatars = findByProps("BOT_AVATARS")
+
+let patches = []
+
+patches.push(before("actionHandler", FD.MESSAGE_CREATE?.find(i => i.name === "MessageStore"), (args: any) => {
+console.log(args)
+                
+            }));
+            
+export const settings = Settings;
+
+export const onLoad = () => {
+    storage.modafk ??= false
+    storage.afk ??= "to afk"
+}
+
+export const onUnload = () => {
+    for (const unregisterCommands of patches) unregisterCommands()
+}
