4142dc882a047376ac290e8bf8d6d378ec376371
kauannre <130103202+kauannre@users.noreply.github.com>
2023-04-19 17:26:42 -0300

    Update index.ts

--- plugins/inviteinfo/index.ts
+++ plugins/inviteinfo/index.ts
@@ -1,4 +1,3 @@
-
 import { registerCommand } from "@vendetta/commands"
 import { logger } from "@vendetta";
 import { findByProps } from "@vendetta/metro"
@@ -6,6 +5,7 @@ import { storage } from '@vendetta/plugin';
 const MessageActions = findByProps("sendMessage", "receiveMessage")
 import { FluxDispatcher } from '@vendetta/metro/common';
 import { before, after} from "@vendetta/patcher"
+
 const FD = FluxDispatcher._actionHandlers._orderedActionHandlers;
 const meuid = findByProps("getCurrentUser").getCurrentUser().id
 const api = findByProps("get", "post");
@@ -13,75 +13,68 @@ const botmsg = findByProps("sendBotMessage");
 const regex = /discord\.gg\/[a-zA-Z0-9\-_]+/g;
 
 function constructMessage(message, channel) {
-    let msg = {
-        id: '',
-        type: 0,
-        content: '',
-        channel_id: channel.id,
-        author: {
-            id: '',
-            username: '',
-            avatar: '',
-            discriminator: '',
-            publicFlags: 0,
-            avatarDecoration: null,
-        },
-        attachments: [],
-        embeds: [],
-        mentions: [],
-        mention_roles: [],
-        pinned: false,
-        mention_everyone: false,
-        tts: false,
-        timestamp: '',
-        edited_timestamp: null,
-        flags: 0,
-        components: [],
-    };
+  let msg = {
+    id: '',
+    type: 0,
+    content: '',
+    channel_id: channel.id,
+    author: {
+      id: '',
+      username: '',
+      avatar: '',
+      discriminator: '',
+      publicFlags: 0,
+      avatarDecoration: null,
+    },
+    attachments: [],
+    embeds: [],
+    mentions: [],
+    mention_roles: [],
+    pinned: false,
+    mention_everyone: false,
+    tts: false,
+    timestamp: '',
+    edited_timestamp: null,
+    flags: 0,
+    components: [],
+  };
 
-    if (typeof message === 'string') msg.content = message;
-    else msg = { ...msg, ...message };
+  if (typeof message === 'string') {
+    msg.content = message;
+  } else {
+    msg = { ...msg, ...message };
+  }
 
-    return msg;
+  return msg;
 };
 
 let patches = []
 
-
-
 const delayedStart = () => {
-patches.push(before("actionHandler", FD.MESSAGE_CREATE?.find(i => i.name === "MessageStore"), async (args: any) => {
-let message = args[0].message;
-let guildId = args[0].guildId;
-let channelId = args[0].channelId;
+  patches.push(before("actionHandler", FD.MESSAGE_CREATE?.find(i => i.name === "MessageStore"), async (args: any) => {
+    let message = args[0].message;
+    let guildId = args[0].guildId;
+    let channelId = args[0].channelId;
 
-const teminvite = regex.test(message.content)
+    const teminvite = regex.test(message.content)
 
-if(teminvite) {
-let convites = string_exemplo.match(regex);
-logger.log(convites)
-
-
-  }})
-
-
-
-
-}
-              return  
-            }));
+    if (teminvite) {
+      let convites = string_exemplo.match(regex);
+      logger.log(convites)
+    }
+  }));
+};
 
 export const onLoad = () => {
-
-FluxDispatcher.dispatch({
-            type: "MESSAGE_CREATE",
-            message: constructMessage('PLACEHOLDER', { id: '0' }),
-        });
-        
-    setTimeout(() => delayedStart(), 300);
+  FluxDispatcher.dispatch({
+    type: "MESSAGE_CREATE",
+    message: constructMessage('PLACEHOLDER', { id: '0' }),
+  });
+  setTimeout(() => delayedStart(), 300);
 }
 
 export const onUnload = () => {
-    for (const unregisterCommands of patches) unregisterCommands()
+  for (const unregisterCommands of patches) {
+    unregisterCommands()
+  }
 }
-
